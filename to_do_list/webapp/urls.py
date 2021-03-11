7ca536139f5b49d6b118cfb0b02f4b28b84773c9
Asman <iasman312@gmail.com>
2021-03-11 15:52:52 +0600

    Создали вьюшку для редактирования задачи

--- to_do_list/webapp/urls.py
+++ to_do_list/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from webapp.views import IndexView, TaskView, TaskCreateView
+from webapp.views import IndexView, TaskView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='task-list'),
     path('create/', TaskCreateView.as_view(), name='task-create'),
     path('<int:pk>/', TaskView.as_view(), name='task-view'),
+    path('<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
 ]
--- to_do_list/webapp/views.py
+++ to_do_list/webapp/views.py
@@ -44,3 +44,34 @@ class TaskCreateView(TemplateView):
             return redirect('task-view', pk=task.id)
         kwargs['form'] = form
         return super().get_context_data(**kwargs)
+
+
+class TaskUpdateView(TemplateView):
+    template_name = 'task_update.html'
+
+    def get_context_data(self, **kwargs):
+        task = get_object_or_404(Task, id=kwargs.get('pk'))
+        form = TaskForm(initial={
+            'summary': task.summary,
+            'description': task.description,
+            'status': task.status,
+            'type': task.type
+        })
+        kwargs['form'] = form
+        kwargs['task'] = task
+        return super().get_context_data(**kwargs)
+
+    def post(self, request, **kwargs):
+        task = get_object_or_404(Task, id=kwargs.get('pk'))
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task.summary = form.cleaned_data.get('summary')
+            task.description = form.cleaned_data.get('description')
+            task.status = form.cleaned_data.get('status')
+            task.type = form.cleaned_data.get('type')
+            task.save()
+            return redirect('task-view', pk=task.id)
+
+        kwargs['form'] = form
+        kwargs['task'] = task
+        return super().get_context_data(**kwargs)
