a9dfb28def22f424f2e7343402ae27d1dbc1fd47
Asman <iasman312@gmail.com>
2021-03-11 13:50:25 +0600

    Создали модель задачи, типа и статуса и сделали миграцию

--- to_do_list/webapp/admin.py
+++ to_do_list/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Task, Status, Type
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'created_at', 'updated_at']
+    list_filter = ['summary']
+    search_fields = ['summary']
+    fields = ['summary', 'status', 'type', 'description']
+    readonly_fields = ['created_at', 'updated_at', 'id']
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
+
--- to_do_list/webapp/models.py
+++ to_do_list/webapp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    summary = models.CharField(max_length=120, null=False, blank=False,
+                               verbose_name='Краткое описание',
+                               default='summary')
+    description = models.TextField(max_length=1000, null=True, blank=True,
+                                   verbose_name='Полное описание')
+    status = models.ForeignKey('webapp.Status', related_name='tasks',
+                               on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', related_name='tasks',
+                             on_delete=models.PROTECT, verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'tasks'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f'{self.id}. {self.summary}: {self.status}, {self.type}'
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=120, null=False, blank=False,
+                            verbose_name='Название')
+
+    class Meta:
+        db_table = 'statuses'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=120, null=False, blank=False,
+                            verbose_name='Название')
+
+    class Meta:
+        db_table = 'types'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+
+    def __str__(self):
+        return f'{self.name}'
