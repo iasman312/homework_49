198562c03758ea1cc2f5e9c10397cd4035f1959c
Asman <iasman312@gmail.com>
2021-03-11 13:51:48 +0600

    убрали defauld и сделали миграцию

--- to_do_list/webapp/models.py
+++ to_do_list/webapp/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     summary = models.CharField(max_length=120, null=False, blank=False,
-                               verbose_name='Краткое описание',
-                               default='summary')
+                               verbose_name='Краткое описание')
     description = models.TextField(max_length=1000, null=True, blank=True,
                                    verbose_name='Полное описание')
     status = models.ForeignKey('webapp.Status', related_name='tasks',
