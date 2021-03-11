0cf45ded861828db1f83cdaff823347989e4667a
Asman <iasman312@gmail.com>
2021-03-11 14:21:28 +0600

    Создали вьюшку для домашней страницы, создали шаблон для этой страницы

--- to_do_list/webapp/views.py
+++ to_do_list/webapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View, TemplateView, RedirectView
 
-# Create your views here.
+from webapp.models import Task
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['tasks'] = Task.objects.all()
+        return super().get_context_data(**kwargs)
