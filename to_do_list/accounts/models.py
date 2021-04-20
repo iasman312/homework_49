76c81b2ca2f2b8a52666e597bd6a26af13c9ec11
asman <iasman312@gmail.com>
2021-04-20 13:59:12 +0600

    Создали модель профиля и сделали миграцию

--- to_do_list/accounts/models.py
+++ to_do_list/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    github = models.URLField(null=True, blank=True, verbose_name='Профиль на GitHub')
+    about = models.TextField(max_length=2000, null=True, blank=True, verbose_name='О себе')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
